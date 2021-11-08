acc61b0568d5ee93f401e48eb0176a604205d9ac
Victor Ruiz <victorruiz524@gmail.com>
2021-11-07 20:04:47 -0600

    challenge

---
+++ js/script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    /* Navbar scroll animations*/
+    let heigthNav = document.querySelector('.nav').clientHeight;
+
+    let bodyRect = document.body.getBoundingClientRect(),
+        elemRect = document.querySelector('footer').getBoundingClientRect(),
+        offset = elemRect.top - bodyRect.top;
+
+    document.addEventListener("scroll", () => {
+        if (this.scrollY > heigthNav) {
+            if (this.scrollY > offset - heigthNav) {
+                document.querySelector('.nav').classList.add('nav-footer');
+            } else {
+                document.querySelector('.nav').classList.remove('nav-footer');
+                document.querySelector('.nav').classList.add('fixed');
+            }
+        } else {
+            document.querySelector('.nav').classList.remove('fixed');
+        }
+    });
+
+    /* Menu action */
+
+    const toggleNav = document.querySelector('.nav .nav__links');
+
+    document.querySelector('.nav__menu').addEventListener('click', (e) => {
+        e.target.classList.toggle('closeMenu');
+        toggleNav.classList.toggle('active');
+    });
+
+}, false);
