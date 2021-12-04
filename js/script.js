0f9ce64dc33217a767f387ae7a7e054795321d7d
Victor Ruiz <victorruiz524@gmail.com>
2021-12-03 20:44:29 -0600

    New styles

--- js/script.js
+++ js/script.js
@@ -1,13 +1,43 @@
+function isMobile() {
+
+    if (navigator.userAgent.match(/Android/i) ||
+        navigator.userAgent.match(/webOS/i) ||
+        navigator.userAgent.match(/iPhone/i) ||
+        navigator.userAgent.match(/iPod/i) ||
+        navigator.userAgent.match(/BlackBerry/i) ||
+        navigator.userAgent.match(/Windows Phone/i)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+function resetNav( size = 'none') {
+    const subList = document.querySelectorAll('.nav__links--list__item--sublist');
+    const nav = document.querySelector('.nav .nav__links');
+
+    if (isMobile()) {
+        nav.style['display'] = 'none';
+        
+        subList.forEach((list) => {
+            list.style['display'] = 'none';
+            if (list.parentElement.classList.contains('active')) {
+                list.parentElement.classList.remove('active');
+            }
+        });
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
 
     /* Navbar scroll animations*/
-    let heigthNav = document.querySelector('.nav').clientHeight;
-
-    let bodyRect = document.body.getBoundingClientRect(),
-        elemRect = document.querySelector('footer').getBoundingClientRect(),
-        offset = elemRect.top - bodyRect.top;
-
     document.addEventListener("scroll", () => {
+        let heigthNav = document.querySelector('.nav').clientHeight;
+
+        let bodyRect = document.body.getBoundingClientRect(),
+            elemRect = document.querySelector('footer').getBoundingClientRect(),
+            offset = elemRect.top - bodyRect.top;
         if (this.scrollY > heigthNav) {
             if (this.scrollY > offset - heigthNav) {
                 document.querySelector('.nav').classList.add('nav-footer');
@@ -24,22 +54,50 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const toggleNav = document.querySelector('.nav .nav__links');
     const navListener = document.querySelector('.nav__menu');
-    
+    const navLinks = document.querySelectorAll('.nav__links--list__item');
 
+    resetNav();
     navListener.addEventListener('click', (e) => {
+
         navListener.classList.toggle('closeMenu');
+
+        if (!navListener.classList.contains('closeMenu')) {
+            resetNav();
+        }
+
         toggleNav.classList.toggle('active');
+        if (toggleNav.classList.contains('active')) {
+            toggleNav.style['display'] = 'block';
+        } else {
+            toggleNav.style['display'] = 'none';
+        }
     });
 
     /* Action Nav link */
 
-    const navLinks = document.querySelectorAll('.nav__links--list__item');
+    if (isMobile()) {
+        navLinks.forEach((link) => {
+            link.addEventListener('click', () => {
+                if (link.classList.contains('active')) {
+                    link.querySelector('ul').style['display'] = 'none';
+                } else {
+                    link.querySelector('ul').style['display'] = 'block';
+                }
+                link.classList.toggle('active');
+            });
+        });    
+    }
+    /* Img arrow menu */
 
-    navLinks.forEach((link) => {
-        link.addEventListener('click', () => {
-            link.classList.toggle('active')
+    const arrowsMenu = document.querySelectorAll('.nav__links--list__item--link img');
+        arrowsMenu.forEach((menu) => {
+            console.log(menu.src);
+            if (isMobile()) {
+                menu.src = './images/icon-arrow-dark.svg'
+            } else {
+                menu.src = './images/icon-arrow-light.svg'
+            }
         })
-    })
 
 
 }, false);
